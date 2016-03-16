3b8d6dc613bb19abbf2c400a986523425f9f1da1
komahan <komahan.cool@gmail.com>
2016-03-16 10:40:36 -0400

    test program changed for multivariate form

--- new/test_runge_kutta.f90
+++ new/test_runge_kutta.f90
@@ -4,107 +4,74 @@
 
 program main
   
-  use abstract_runge_kutta
   use explicit_runge_kutta
-  use implicit_runge_kutta
   
   implicit none
 
   integer :: i, kk
 
-  integer, parameter :: N = 10
-  real(8), parameter :: h =  1.0d0
+  integer, parameter :: M = 1
+  real(8), parameter :: h =  1.0d-1
   real(8), parameter :: tinit = 0.0d0
-
-  type(DIRK):: DIRKOBJ
-  type(IRK) :: IRKOBJ
+  real(8), parameter :: tfinal = 5.0d0
+  real(8), allocatable, dimension(:,:,:) :: q, qdot
+  integer :: nargs, j, N
+  logical :: descriptor = .true.
+  character(len=5) :: str
   type(ERK) :: ERKOBJ
+
+  !-------------------------------------------------------------------!
+  ! get the command line arguments
+  !-------------------------------------------------------------------!
   
-  real(8) :: q(4, N+1) = 0.0d0, &
-       & qdot(4, N+1) = 0.0d0, &
-       & error(4, 2, N+1) = 0.0d0
+  nargs = command_argument_count()
+  do j = 1, nargs
+     call get_command_argument(j, str)
+     if (str .eq. "false") then
+        descriptor = .false.
+     end if
+  end do
+  
+  !-------------------------------------------------------------------!
+  ! find the number of steps needed for time marching
+  !-------------------------------------------------------------------!
+
+  N = (tfinal - tinit)/h
+
+  allocate(q(4, N+1,M))
+  allocate(qdot(4, N+1,M))
   
   !-------------------------------------------------------------------!
   ! Explicit Runge Kutta
   !-------------------------------------------------------------------!
-
-  q = 0.0d0; q(:,1) = 0.0d0; qdot(:,1)=1.0d0
   
-  do kk = 1, 4
+  open(unit=90, file='erk.dat')
 
+  q = 0.0d0; q(:,1,:) = 0.0d0; qdot(:,1,:)=1.0d0
+
+  do kk = 1, 3
+     
+     if (kk.eq.1) open(unit=90, file='erk1.dat')
+     if (kk.eq.2) open(unit=90, file='erk2.dat')
+     if (kk.eq.3) open(unit=90, file='erk3.dat')
+     
      ! Test for each RK stage
+     
+     ERKOBJ % descriptor_form = descriptor
+
      call ERKOBJ  % initialize(h=h,tinit=tinit,num_stages=kk)
-     call ERKOBJ  % integrate(q(kk,:), qdot(kk,:), N)
+     call ERKOBJ  % integrate(q(kk,:,:), qdot(kk,:,:), N)
      call ERKOBJ  % finalize()
-
+     
      ! Find the error
      do i = 1, N + 1
-        error(kk, 1, i) = abs(q(kk,i) - exact(i, tinit, h))
-     end do
-     
-     do i = 1, N + 1
-        error(kk, 2, i) = abs(qdot(kk,i) - cos(dble(i-1)*ERKOBJ % h))
-     end do
-     
-  end do
-
-  write (*, '(a,4E15.8)') "ERK :",(norm2(error(i, 1, :)), i = 1, 4)
-  write (*, '(a,4E15.8)') "ERK :",(norm2(error(i, 2, :)), i = 1, 4)
-
-  !-------------------------------------------------------------------!
-  ! Implicit Runge Kutta
-  !-------------------------------------------------------------------!
-  
-  q = 0.0d0; q(:,1) = 0.0d0; qdot(:,1)=1.0d0
-
-  do kk = 1, 3
-
-     ! Test for each RK stage
-     call IRKOBJ % initialize(h=h,tinit=tinit,num_stages=kk)
-     call IRKOBJ % integrate(q(kk,:), qdot(kk,:), N)
-     call IRKOBJ % finalize()
-     
-     ! Find the error
-     do i = 1, N +1
-        error(kk, 1, i) = abs(q(kk,i) - exact(i, tinit, h))
+        write(90, *)  tinit + dble(i-1)*h, (q(kk,i,j),j=1,M)
      end do
 
-     do i = 1, N +1
-        error(kk, 2, i) = abs(qdot(kk,i) - cos(dble(i-1)*ERKOBJ % h))
-     end do
+  close(90)
 
   end do
-
-  write (*, '(a,4E15.8)') "IRK :", (norm2(error(i, 1, :)), i = 1, 3)
-  write (*, '(a,4E15.8)') "IRK :", (norm2(error(i, 2, :)), i = 1, 3)
-
-  !-------------------------------------------------------------------!
-  ! Diagonally Implicit Runge Kutta
-  !-------------------------------------------------------------------!
   
-  q = 0.0d0; q(:,1) = 0.0d0; qdot(:,1)=1.0d0
-  
-  do kk = 1, 3
-
-     ! Test for each RK stage
-     call DIRKOBJ  % initialize(h=h,tinit=tinit,num_stages=kk)
-     call DIRKOBJ  % integrate(q(kk,:), qdot(kk,:), N)
-     call DIRKOBJ  % finalize()
-
-     ! Find the error
-     do i = 1, N +1
-        error(kk, 1, i) = abs(q(kk,i) - exact(i, tinit, h))
-     end do
-
-     do i = 1, N +1
-        error(kk, 2, i) = abs(qdot(kk,i) - cos(dble(i-1)*ERKOBJ % h))
-     end do
-
-  end do
-
-  write (*, '(a,4E15.8)') "DIRK:", (norm2(error(i, 1, :)), i = 1, 3)
-  write (*, '(a,4E15.8)') "DIRK:", (norm2(error(i, 2, :)), i = 1, 3)
-
 contains
 
   real(8) function exact(k, tinit, h)
@@ -120,8 +87,3 @@ contains
   end function exact
 
 end program main
-
-!!$        print *, tinit + dble(i-1)*h, q(kk,i), &
-!!$             & exact(i, tinit, h) , &
-!!$             & q(kk,i) - exact(i, tinit, h), &
-!!$             & qdot(kk,i) - cos(dble(i-1)*h)
